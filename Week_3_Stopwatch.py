7f097bb8ec5d7e2de7da5a7cd5a4983660d40c0b
aabishkar <aabishkar_kc@cable.comcast.com>
2014-11-04 19:02:28 -0500

    reformattted as per python standards

--- Week_3_Stopwatch.py
+++ Week_3_Stopwatch.py
@@ -1,13 +1,14 @@
 # template for "Stopwatch: The Game"
-import simplegui
+import simpleguitk as simplegui
 
 # define global variables
 time = 0
 interval = 100
 total_stops = 0
-succes_stops = 0
+success_stops = 0
 A = B = C = D = 0
-stop = True
+stopped = True
+
 
 # define helper function format that converts time
 # in tenths of seconds into formatted string A:BC.D
@@ -20,42 +21,47 @@ def format(t):
     msg = str(A) + ":" + str(B) + str(C) + "." + str(D)
     return msg
 
-# define event handlers for buttons; "Start", "Stop", "Reset"
+
+# define event handlers for buttons; "Start", "stop", "Reset"
 def start():
-    global time, stop
-    stop = False
+    global time, stopped
+    stopped = False
     timer.start()
 
+
 def stop():
-    global total_stops, succes_stops, stop
-    if stop == False :
-        if time % 10 == 0 and time != 0 :
-            succes_stops += 1
+    global total_stops, success_stops, stopped
+    if not stopped:
+        if time % 10 == 0 and time != 0:
+            success_stops += 1
             total_stops += 1
-        elif time != 0 :
+        elif time != 0:
             total_stops += 1
-    stop = True
+    stopped = True
     timer.stop()
 
+
 def reset():
-    global time, succes_stops, total_stops
+    global time, success_stops, total_stops, stopped
     time = 0
     total_stops = 0
-    succes_stops = 0
-    stop = True
+    success_stops = 0
+    stopped = True
     timer.stop()
+
 
 # define event handler for timer with 0.1 sec interval
 def tick():
     global time
     time += 1
 
+
 # define draw handler
 def draw(canvas):
     canvas.draw_text(format(time), (80, 125), 42, "white")
-    canvas.draw_text(str(succes_stops) + '/' + str(total_stops), (150,50), 24, "Red")
+    canvas.draw_text(str(success_stops) + '/' + str(total_stops), (150, 50), 24, "Red")
 
-    
+
 # create frame
 frame = simplegui.create_frame("Timer", 200, 200)
 
@@ -72,4 +78,4 @@ frame.start()
 reset()
 
 # Please remember to review the grading rubric
-#http://www.codeskulptor.org/#user38_7bD4fRnD5I_0.py
+# http://www.codeskulptor.org/#user38_7bD4fRnD5I_0.py
